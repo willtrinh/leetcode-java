a9ac97217f0b3a58942c553c9c35b0fcc726dce6
willtrinh <williamltrinh@gmail.com>
2020-11-20 21:48:09 -0800

    add problems

---
+++ array/TwoSum.java
@@ -0,0 +1,44 @@
+/* 
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+package array;
+
+import java.util.Map;
+
+public class TwoSum {
+    // Time: O(n), Space: O(n)
+    public int[] twoSum(int[] nums, int target) {
+        // create a map of complements
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            // check if current element's complement is in the table
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
