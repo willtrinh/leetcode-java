f7aae6f3fb498cc0eec1d49330d11065a325ff1d
willtrinh <williamltrinh@gmail.com>
2020-11-20 16:06:20 -0800

    add MoveZeroes

---
+++ two-pointers/MoveZeroes.java
@@ -0,0 +1,29 @@
+// Given an array nums, write a function to move all 0's to the end of it
+// while maintaining the relative order of the non-zero elements.
+/* 
+Example:
+Input: [0,1,0,3,12]
+Output: [1,3,12,0,0]
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+
+*/
+class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        // Shift non-zero values as far forward as possible
+        // Fill remaining space with zeros
+        if (nums == null || nums.length == 0)
+            return;
+        int index = 0;
+        for (int num : nums) {
+            if (num != 0)
+                nums[index++] = num;
+        }
+
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+}
