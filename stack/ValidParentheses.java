34c2f5de02537df7bb167e32e1fcb1ce62aed28a
willtrinh <williamltrinh@gmail.com>
2020-11-21 15:06:18 -0800

    add ValidParentheses

---
+++ stack/ValidParentheses.java
@@ -0,0 +1,46 @@
+/* 
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+Example 4:
+
+Input: s = "([)]"
+Output: false
+*/
+package stack;
+
+// Basic idea: create a stack that store the closing type of brackets.
+// Every time we encounter an opening brackets `(, {. [` push the corresponding closing bracket into the stack
+// When we encounter a closing bracket, stack.pop() would be equal to the closing bracket for it to be valid.
+// If stack.pop() != closing bracket, return false.
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false;
+        }
+        return stack.isEmpty();
+    }
+}
